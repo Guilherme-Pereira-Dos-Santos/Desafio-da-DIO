b6608c27e99eb366c01ff0b6e9b8d93ed9c0ab38
Guilherme-Pereira-Dos-Santos <guilhermepereira280207@gmail.com>
2024-05-02 16:40:39 -0300

    Ultima versão do desafio DIO

--- index.js
+++ index.js
@@ -2,33 +2,34 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
-  
+  readline.question('Qual o seu nome? ', Nome => {
+ 
   readline.question('quanto de xp você tem? ', xpPlayer => { 
 
     if (xpPlayer>0 && xpPlayer<=1000){
-        console.log("Seu nivel é ferro")
+        console.log("O heroi de nome: " + Nome + " está no nível ferro")
     }
     else if( xpPlayer>0 && xpPlayer>=1001 && xpPlayer<=2000){
-        console.log("Seu nivel é Bronze")
+        console.log("O heroi de nome: " + Nome +  " está no nível Bronze")
     }
     else if(xpPlayer>0 && xpPlayer>=2001 && xpPlayer<=5000){
-        console.log("Seu nivel é Prata")
+        console.log("O heroi de nome: " + Nome + " está no nível Prata")
     }
     else if(xpPlayer>0 && xpPlayer>=5001 && xpPlayer<=7000){
-        console.log("Seu nivel é Ouro")
+        console.log("O heroi de nome: " + Nome + " está no nível Ouro")
     }
     else if(xpPlayer>0 && xpPlayer>0 && xpPlayer>=7001 && xpPlayer<=8000){
-        console.log("Seu nivel é Platina")
+        console.log("O heroi de nome: " + Nome + " está no nível Platina")
     }
     else if(xpPlayer>0 && xpPlayer>=8001 && xpPlayer<=9000){
-        console.log("Seu nivel é Ascendente")
+        console.log("O heroi de nome: " + Nome + " está no nível Ascendente")
     }
     else if(xpPlayer>0 && xpPlayer>=9001 && xpPlayer<=10000){
-        console.log("Seu nivel é Imortal")
+        console.log("O heroi de nome: " + Nome + " está no nível Imortal")
     }
     else if (xpPlayer>=10001 ) 
     {
-        console.log("Seu nivel é platina")
+        console.log(Nome + " Seu nivel é platina")
     }
     else if (xpPlayer<=0)
     {
@@ -37,7 +38,7 @@ const readline = require('readline').createInterface({
 
 console.log("Aperte Ctrl+C para para o progama")
   });
-  
+});
 
 
 /*let xpPlayer = 5100
